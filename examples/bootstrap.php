1e8d90c05d82f5918f7f5eff06db1c7347e3f383
Mihai Blebea <mihai@getchip.uk>
2020-06-16 15:31:28 +0100

    Updates gitignore and env file

--- examples/bootstrap.php
+++ examples/bootstrap.php
@@ -4,4 +4,4 @@ require_once __DIR__ . '/../vendor/autoload.php';
 
 use Chip\ComplyAdvantageApi\ComplyAdvantageApi;
 
-$client = new ComplyAdvantageApi('itFiDoVKrVxnb1FBV0i1OE9WgFJgHZbx');
+$client = new ComplyAdvantageApi(getenv('API_KEY'));
